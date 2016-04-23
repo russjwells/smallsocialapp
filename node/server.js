ba29d5fbdadbbf23488770123c92701c04863186
Russ Wells <russ@russdigital.com>
2016-04-22 23:51:44 -0700

    serve posts.html through node

--- node/server.js
+++ node/server.js
@@ -5,6 +5,10 @@ var Post = require('./models/post')
 var app = express()
 app.use(bodyParser.json())
 
+app.get('/', function (req, res) {
+	res.sendfile('layouts/posts.html')
+})
+
 app.get('/api/posts',  function (req, res, next) {
 	Post.find(function(err, posts) {
 		if (err) { return next(err) }
