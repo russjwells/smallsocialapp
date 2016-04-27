fa90de3b7673d7515aed400f9ffdc79db2851139
Russ Wells <russ@russdigital.com>
2016-04-26 22:53:38 -0700

    compartmentalized the elements of node and angular appropriately

--- node/server.js
+++ node/server.js
@@ -9,26 +9,8 @@ app.get('/', function (req, res) {
 	res.sendfile('layouts/posts.html')
 })
 
-app.get('/api/posts',  function (req, res, next) {
-	Post.find()
-	.sort('-date')
-	.exec(function(err, posts) {
-		if (err) { return next(err) }
-			res.json(posts)
-	})
-})
-
-var Post = require('./models/post')
-app.post('/api/posts', function (req, res, next) {
-	var post = new Post({
-		username: req.body.username,
-		body: req.body.body
-	})
-	post.save(function (err, post) {
-		if (err) { return next(err) }
-		res.json(201, post)
-	})
-})
+app.use('/api/posts', require('./controllers/api/posts'))
+app.use( require('./controllers/static'))
 
 app.listen(3000, function () {
 	console.log('Server listening on', 3000)
