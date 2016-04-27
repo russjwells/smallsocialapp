caccaed35c12f51b7cd4aee858bbba977a922d19
Russ Wells <russ@russdigital.com>
2016-04-27 13:01:34 -0700

    began gulp

---
+++ node/gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp')
+
+gulp.task('welcome', function () {
+	console.log('welcome to gulp!')
+})
+
+gulp.task('hello', ['welcome'], function () {
+	console.log('hello world')
+})
+
