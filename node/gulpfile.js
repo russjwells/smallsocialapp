8dbf5b163953949dfd97d4edba25479722799efe
Russ Wells <russ@russdigital.com>
2016-04-27 13:28:22 -0700

    gulping

--- node/gulpfile.js
+++ node/gulpfile.js
@@ -1,10 +1,8 @@
 var gulp = require('gulp')
+var concat = require('gulp-concat')
 
-gulp.task('welcome', function () {
-	console.log('welcome to gulp!')
+gulp.task('js', function () {
+	gulp.src('ng/**/*.js')
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest('assets'))
 })
-
-gulp.task('hello', ['welcome'], function () {
-	console.log('hello world')
-})
-
