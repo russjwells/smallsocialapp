9e40f551950dee17166587688099d41a6233521a
Russ Wells <russ@russdigital.com>
2016-05-10 13:54:35 -0700

    fixed the parse error

--- node/gulpfile.js
+++ node/gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp')
-
+var nodemon = require('gulp-nodemon')
 
 //require the other gulp files
 var fs = require('fs')
@@ -7,4 +7,5 @@ fs.readdirSync(__dirname + '/gulp').forEach(function (task) {
 	require('./gulp/' + task)
 })
 
+gulp.task('dev', ['watch:css', 'watch:js', 'dev:server'])
 
