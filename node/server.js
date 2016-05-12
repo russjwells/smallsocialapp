cba6415b158a7ed5745b7a86d25c3947d99e1e32
Russ Wells <russ@russdigital.com>
2016-05-11 22:01:08 -0700

    Authentication / login

--- node/server.js
+++ node/server.js
@@ -12,6 +12,9 @@ app.use(bodyParser.json())
 app.use('/api/posts', require('./controllers/api/posts'))
 app.use( require('./controllers/static'))
 
+app.use('/api/sessions', require('./controllers/api/sessions'))
+app.use('/api/users', require('./controllers/api/users'))
+
 app.listen(3000, function () {
-	console.log('Server listening on', 3000)
+	console.log('Welcome to SSAPP Server listening on', 3000)
 })
