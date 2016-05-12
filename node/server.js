8f03ec03efbbb8ffd8a19e5c130bf6f6403bfca8
Russ Wells <russ@russdigital.com>
2016-05-12 00:03:06 -0700

    only logged in users can post now/authentication of social posts

--- node/server.js
+++ node/server.js
@@ -8,10 +8,9 @@ app.use(bodyParser.json())
 //app.get('/', function (req, res) {
 //	res.sendfile('layouts/posts.html')
 //})
-
+app.use(require('./auth'))
 app.use('/api/posts', require('./controllers/api/posts'))
 app.use( require('./controllers/static'))
-
 app.use('/api/sessions', require('./controllers/api/sessions'))
 app.use('/api/users', require('./controllers/api/users'))
 
