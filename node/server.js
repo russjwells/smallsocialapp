48aa00d8b76dfab2674f86d38e2ca050de89e695
Russ Wells <russ@russdigital.com>
2016-05-12 18:07:13 -0700

    websockets started

--- node/server.js
+++ node/server.js
@@ -14,6 +14,8 @@ app.use( require('./controllers/static'))
 app.use('/api/sessions', require('./controllers/api/sessions'))
 app.use('/api/users', require('./controllers/api/users'))
 
-app.listen(3000, function () {
+
+var server = app.listen(3000, function () {
 	console.log('Welcome to SSAPP Server listening on', 3000)
 })
+require('./websockets').connect(server)
